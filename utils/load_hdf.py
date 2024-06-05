1dda9db236335329c235e99c65e6d8002727fd97
CenJim <94654311+CenJim@users.noreply.github.com>
2024-06-05 16:32:24 +0100

    load_hdf reverse row and column

--- utils/load_hdf.py
+++ utils/load_hdf.py
@@ -5,19 +5,19 @@ import hdf5plugin
 
 def chunk_2d_array(array, chunk_size):
     chunked_array = []
-    for i in range(0, len(array[0]), chunk_size):
-            chunked_array.append(array[:, i:i+chunk_size])
+    for i in range(0, len(array), chunk_size):
+            chunked_array.append(array[i:i+chunk_size, :])
     return chunked_array
 
 def get_dataset(file_path, width, height, num_events_per_pixel):
     # open HDF5 file
     with h5py.File(file_path, 'r') as f:
         # obtain the t x y p
-        dataset = np.zeros(shape=(4, len(f['events/p'])))
-        dataset[0] = f['events/t'][()]
-        dataset[1] = f['events/x'][()]
-        dataset[2] = f['events/y'][()]
-        dataset[3] = f['events/p'][()]
+        dataset = np.zeros(shape=(len(f['events/p']), 4))
+        dataset[:, 0] = f['events/t'][()]
+        dataset[:, 1] = f['events/x'][()]
+        dataset[:, 2] = f['events/y'][()]
+        dataset[:, 3] = f['events/p'][()]
 
         return chunk_2d_array(dataset, int(width*height*num_events_per_pixel))
 
