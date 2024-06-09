62c978818e85edee902901f3289287f9fa8e96ff
Xiujing Li <s2491540@w7128.see.ed.ac.uk>
2024-06-09 03:23:05 +0100

    project.sh

--- project.sh
+++ project.sh
@@ -2,6 +2,6 @@
 source ~/.bashrc
 conda activate snnrec 
 python rec_snn.py -network EVSNN_LIF_final -path_to_pretrain_models ./pretrained_models/EVSNN.pth \
-    -path_to_event_files /home/s2491540/dataset/DSEC/test/thun_01_a/thun_01_a_events_left/ \
-    -height 480 -width 640
+    -path_to_event_files /home/s2491540/dataset/IJRR/poster_6dof_txt/events.txt \
+    -height 180 -width 240
 
